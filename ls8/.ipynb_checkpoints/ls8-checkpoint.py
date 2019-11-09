f02c8cbb0e9f2d0b908b4c505e8a9a4433f47263
badCompany55 <zachirvin45@gmail.com>
2019-11-09 12:55:10 -0600

    MVP COMPLETE CMP with values for flags equal, less and else cases JMP JEQ JNE implemented run test file returns 1 4 5

---
+++ ls8/.ipynb_checkpoints/ls8-checkpoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import getopt, sys
+
+"""Main."""
+from cpu import *
+
+cpu = CPU()
+#
+#cpu.load()
+#cpu.run()
+
+all_command_args = sys.argv
+
+arg_list = all_command_args[1:]
+
+unixOptions = "hf"
+gnuOptions = ["help", "file="]
+
+try:
+    args, values = getopt.getopt(arg_list, unixOptions, gnuOptions)
+    
+except getopt.error as err:
+        print(str(err))
+        sys.exit(2)
+        
+for curr_arg, curr_val in args:
+    if curr_arg in ("-f", "--file"):
+        print(f'File to Load, {curr_val}')
+        cpu.load(curr_val)
+        cpu.run()
+    elif curr_arg in ("-h", "--help"):
+        print("Displaying the Help Information")
